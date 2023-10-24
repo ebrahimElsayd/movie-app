7cd795f417d253536ea2b2bed80feea7b2556aef
Shaimaaabdelmoaen <sa3073@fayoum.edu.eg>
2023-10-24 10:49:10 +0300

    + initial commit + homeScreen

---
+++ lib/Ui/HomeScreen/homeScreen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:movies_app/Ui/HomeScreen/Browse/Browse.dart';
+import 'package:movies_app/Ui/HomeScreen/Home/Home.dart';
+import 'package:movies_app/Ui/HomeScreen/Search/Search.dart';
+import 'package:movies_app/Ui/HomeScreen/WatchList/WatchList.dart';
+
+class homeScreen extends StatefulWidget{
+  static const String routename='home';
+
+  @override
+  State<homeScreen> createState() => _homeScreenState();
+}
+
+class _homeScreenState extends State<homeScreen> {
+  int selectTapIndex=0;
+  List<Widget>taps=[
+    Home(),
+    Search(),
+    Browse(),
+    WatchList()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+     backgroundColor: Theme.of(context).primaryColor,
+     bottomNavigationBar: BottomNavigationBar(
+       onTap: (index){
+         setState(() {
+           selectTapIndex=index;
+         });
+       },
+       currentIndex: selectTapIndex,
+       items: [
+         BottomNavigationBarItem(
+           backgroundColor: Colors.black,
+             icon:ImageIcon(AssetImage('assets/images/Home_icon.png'),
+             ),
+           label: 'Home'
+         ),
+         BottomNavigationBarItem(
+             backgroundColor: Theme.of(context).primaryColor,
+             icon:ImageIcon(AssetImage('assets/images/Search_icon.png')
+             ),
+           label: 'Search'
+         ),
+         BottomNavigationBarItem(
+             backgroundColor: Theme.of(context).primaryColor,
+             icon:ImageIcon(AssetImage('assets/images/Downloads_icon.png')
+             ),
+           label: 'Browse'
+         ),
+         BottomNavigationBarItem(
+             backgroundColor: Theme.of(context).primaryColor,
+             icon:ImageIcon(AssetImage('assets/images/watchList_icon.png')
+             ),
+           label: 'WatchList'
+         )
+
+       ],
+     ),
+     body:taps[selectTapIndex] ,
+
+
+   );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/Ui/HomeScreen/homeScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        homeScreen.routename : (_) =>homeScreen(),
+      },
+      initialRoute: homeScreen.routename,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF1A1A1A),
+          primary: Color(0xFF1A1A1A),
+          secondary: Color(0xFF1A1A1A),
+          onPrimary: Colors.white,
+          onSecondary: Colors.black
+        ),
+        primaryColor: Color(0xFF1A1A1A),
+          secondaryHeaderColor: Colors.black,
+          scaffoldBackgroundColor:Colors.transparent ,
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          selectedItemColor: Color(0xFFFBAF22),
+          unselectedItemColor: Colors.white,
+          selectedIconTheme: IconThemeData(
+              size: 32
+          ),
+        ),
+          useMaterial3: true
+      ),
+    );
+  }
+}

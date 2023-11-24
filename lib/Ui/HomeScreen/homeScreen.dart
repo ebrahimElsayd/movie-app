07e09a082f5ac926d7145d2594955b7c27d0d2e3
E.Alaraby <emadaarby190@gmail.com>
2023-11-25 01:28:12 +0200

    done

--- lib/Ui/HomeScreen/homeScreen.dart
+++ lib/Ui/HomeScreen/homeScreen.dart
@@ -67,7 +67,7 @@ class _HomeState extends State<Home> {
      ),
       body:
       // taps[selectTapIndex] ,
-     Expanded(child: IndexedStack(
+     IndexedStack(
        index: selectTapIndex,
        children: const <Widget>[
          HomeScreen(),
@@ -75,7 +75,7 @@ class _HomeState extends State<Home> {
          Browse(),
          WatchList()
        ],
-     ))
+     )
 
 
    );
